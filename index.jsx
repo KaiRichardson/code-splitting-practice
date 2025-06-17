3f5c4266084e5d0f97ac9eea69aad4ec24241ff9
Kai Richardson <kairichardson2000@gmail.com>
2025-06-17 16:02:44 -0500

    finishing practice

--- index.jsx
+++ index.jsx
@@ -1,47 +1,48 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import ProductsList from "./ProductsList"
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import ProductsList from "./ProductsList";
+
+const ProductsList = React.lazy(() => {
+	return import("./ProductsList");
+});
 
 function App() {
-  const [count, setCount] = React.useState(0)
-  const [showProducts, setShowProducts] = React.useState(false)
+	const [count, setCount] = React.useState(0);
+	const [showProducts, setShowProducts] = React.useState(false);
 
-  function increment() {
-    setCount(prevCount => prevCount + 1)
-  }
+	function increment() {
+		setCount((prevCount) => prevCount + 1);
+	}
 
-  function decrement() {
-    setCount(prevCount => prevCount - 1)
-  }
+	function decrement() {
+		setCount((prevCount) => prevCount - 1);
+	}
 
-  return (
-    <>
-      <h1>The current count is {count}</h1>
-      <button className="button" onClick={decrement}>
-        -
-            </button>
-      <button className="button" onClick={increment}>
-        +
-            </button>
-      <br />
-      <br />
-      <button
-        className="button"
-        onClick={() => setShowProducts(prev => !prev)}
-      >
-        Show Products
-            </button>
-      <br />
-      <br />
-      <div className="products-list">
-        {showProducts && <ProductsList />}
-      </div>
-    </>
-  )
+	return (
+		<>
+			<h1>The current count is {count}</h1>
+			<button className='button' onClick={decrement}>
+				-
+			</button>
+			<button className='button' onClick={increment}>
+				+
+			</button>
+			<br />
+			<br />
+			<button className='button' onClick={() => setShowProducts((prev) => !prev)}>
+				Show Products
+			</button>
+			<br />
+			<br />
+			<React.Suspense fallback={<h2>Loading...</h2>}>
+				<div className='products-list'>{showProducts && <ProductsList />}</div>
+			</React.Suspense>
+		</>
+	);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
